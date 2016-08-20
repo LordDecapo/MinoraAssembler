70bc539699f72f64eed59e39e01e49503418781a
lorddecapo <lorddecapo@gmail.com>
2016-08-20 19:21:37 -0400

    Working assembler needs format control and output writing

--- src/main.py
+++ src/main.py
@@ -31,52 +31,155 @@ except Exception as err:
 
 #Scan, auto add Nop buffs here
 
-OpFormat0 =  { "ADD"  : "0000"
-        , "ADDC" : "0001"
-        , "SUB"  : "0010"
-        , "SUBB" : "0011"
-        , "AND"  : "0100"
-        , "NAND" : "0101"
-        , "XOR"  : "0110"
-        , "XNOR" : "0111"
-        , "OR"   : "1000"
-        , "NOR"  : "1001"
-        , "LD"   : "10100"
-        , "ST"   : "10110"
-        , "LDIO" : "10101"
-        , "STIO" : "10111"
-        , "EXT"  : "11000"
-        , "EXTT" : "11010"
-        , "LIMM" : "11001"
-        , "JUMP" : "11100"
-        , "SHFL" : "0100"
-        , "SHFR" : "0101"
-        , "ADFN" : "10010"
-        , "RMFN" : "10001"}
-OpFormat1 =  {
-        }
 
+
+
+
+#Dictionaries of common instruction formats.
+Logical = { "ADD"  : "0000"
+          , "ADDC" : "0001"
+          , "SUB"  : "0010"
+          , "SUBB" : "0011"
+          , "AND"  : "0100"
+          , "NAND" : "0101"
+          , "XOR"  : "0110"
+          , "XNOR" : "0111"
+          , "OR"   : "1000"
+          , "NOR"  : "1001"
+          , "SHFL" : "0100"
+          , "SHFR" : "0101"}
+
+Memory =  { "LOAD" : "10100"
+          , "STOR" : "10110"
+          , "LDIO" : "10101"
+          , "STIO" : "10111"
+          , "LIMM" : "11001"
+          , "JUMP" : "11100"
+          , "ADFN" : "10010"}
+
+VarBuff = { "EXT"  : "11000"
+          , "EXTT" : "11010"
+          , "MOVA" : "00001"
+          , "MOVB" : "00000"
+          , "INC"  : "00100"
+          , "DEC"  : "00110"
+          , "FUNC" : "10000"
+          , "RMFN" : "10001"}
 
 ilines = source.readlines()
 
 for i in ilines:
-    tok = i.strip("\n").split()
+    tok = i.strip('\n').split()
     if tok == ['END']:
         break
-#    if tok[0] == "BRCH" or "MON"
 
     if tok != []:
-        #print(tok)
-        v0 = str(bin(int(tok[1])))
-        v1 = str(bin(int(tok[2])))
 
-        v0 = v0[2:]
-        v1 = v1[2:]
+        length = len(tok)
 
-        Op = OpFormat1[str(tok[0])]
+    #Decode 1st variable in instruction.
+        if length == 2:
+            #print(tok)
+            v0 = str(bin(int(tok[1])))
+            v0 = v0[2:]
 
-        Inst = Op + v0 + v1
-        print(Inst)
+    #VarBuff are the only instructions
+    #that take just 1 variable.
+            if tok[0] in VarBuff:
+                Op = VarBuff[str(tok[0])]
+                Inst0 = Op + v0
+                Inst1 = '00000000'
+                print(tok[0])
+                print(Inst0)
+                print(Inst1)
+
+            elif tok[0] == 'HIGH':
+                Inst0 = '00100' + v0
+                Inst1 = '00000010'
+                print(tok[0])
+                print(Inst0)
+                print(Inst1)
+
+            elif tok[0] == 'LOW':
+                Inst0 = '00100' + v0
+                Inst1 = '00000011'
+                print(tok[0])
+                print(Inst0)
+                print(Inst1)
+
+            elif tok[0] == 'BADD':
+                Inst0 = '00100' + v0
+                Inst1 = '00000001'
+                print(tok[0])
+                print(Inst0)
+                print(Inst1)
+
+    #Decode 2nd variable in instruction.
+        elif length == 3:
+            v0 = str(bin(int(tok[1])))
+            v0 = v0[2:]
+            v1 = str(bin(int(tok[2])))
+            v1 = v1[2:]
+
+            if tok[0] in Logical:
+                Op = Logical[str(tok[0])]
+                Inst0 = Op + v0 + v1
+                print(tok[0])
+                print(Inst0)
+
+            elif tok[0] in Memory:
+                Op = Memory[str(tok[0])]
+                Inst0 = Op + v0
+                Inst1 = v1
+                print(tok[0])
+                print(Inst0)
+                print(Inst1)
+
+            elif tok[0] == 'PTRL':
+                Inst0 = '00010' + v0
+                Inst1 = '00000' + v1
+                print(tok[0])
+                print(Inst0)
+                print(Inst1)
+
+            elif tok[0] == 'PTRS':
+                Inst0 = '00010' + v0
+                Inst1 = '00001' + v1
+                print(tok[0])
+                print(Inst0)
+                print(Inst1)
+
+            elif tok[0] == 'NOP':
+                Inst0 = '00000000'
+                print(tok[0])
+                print(Inst0)
+
+            elif tok[0] == 'HALT':
+                Inst0 = '11011000'
+                print(tok[0])
+                print(Inst0)
+
+    #decodes the 3rd variable in the instruction.
+        elif length == 4:
+            v0 = str(bin(int(tok[1])))
+            v0 = v0[2:]
+            v1 = str(bin(int(tok[2])))
+            v1 = v0[2:]
+            v2 = str(bin(int(tok[3])))
+            v2 = v1[2:]
+
+            if tok[0] == 'MON':
+                Inst0 = '111' + v0 + v1
+                print(tok[0])
+                print(Inst0)
+                print(Inst1)
+
+            elif tok[0] == 'BRCH':
+                Inst0 = '111' + v0 + v1
+                Inst1 = '111' + v0 + v1
+                print(tok[0])
+                print(Inst0)
+                print(Inst1)
 
 source.close()
 dest.close()
